5f02f7971ecb8065e511b2e8a230c5c300475643
Michael Ramm <micramm@gmail.com>
2014-04-03 22:00:08 -0700

    adding sampleWidget in tutorial

---
+++ tutorial/sampleWidget.py
@@ -0,0 +1,28 @@
+from twisted.internet.defer import inlineCallbacks
+from PyQt4 import QtGui
+
+class sampleWidget(QtGui.QWidget):
+    def __init__(self, reactor, parent=None):
+        super(sampleWidget, self).__init__(parent)
+        self.reactor = reactor
+        self.connect()
+    
+    @inlineCallbacks
+    def connect(self):
+        #make an asynchronous connection to LabRAD
+        from labrad.wrappers import connectAsync
+        self.cxn = yield connectAsync()
+    
+    def closeEvent(self, x):
+        #stop the reactor when closing the widget
+        self.reactor.stop()
+
+if __name__=="__main__":
+    #join Qt and twisted event loops
+    a = QtGui.QApplication( [] )
+    import qt4reactor
+    qt4reactor.install()
+    from twisted.internet import reactor
+    widget = sampleWidget(reactor)
+    widget.show()
+    reactor.run()
