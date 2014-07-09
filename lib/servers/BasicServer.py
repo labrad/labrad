1b224241c3ae857024e9786fc19cc1460647dfcb
Andrew Jayich <jayich@gmail.com>
2014-07-09 15:01:44 -0700

    Made an even more basic server for testing.  It has a function faux_echo which has the same functionality as echo, but is not inherited from the LabradServer class.

---
+++ lib/servers/BasicServer.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+### BEGIN NODE INFO
+[info]
+name = Basic Server
+version = 1.0
+description = 
+instancename = BasicServer
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 20
+### END NODE INFO
+"""
+
+from labrad.server import LabradServer, setting, Signal
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+import labrad
+
+class BasicServer(LabradServer):
+    """
+    Basic Server
+    """
+    
+    name = 'Basic Server'
+
+
+
+    @setting(1, 'faux_echo', string='s', returns='s')
+    def fauxEcho(self, c, string):
+        """
+        """
+        return string
+
+if __name__ == "__main__":
+    from labrad import util
+    util.runServer(BasicServer())
